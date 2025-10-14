6eebfca7a7277cb36a5408390162e375f1f6f707
alizain-patel <alizain.patel@powerweave.com>
2025-10-14 17:54:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # === Load live JSON from GitHub ===
 # Replace with your actual GitHub username and repo name
-DATA_FILE = "https://raw.githubusercontent.com/alizain-patel/shifts-online/refs/heads/main/user_status_dashboard.json"
+DATA_FILE = "https://raw.githubusercontent.com/alizain-patel/shifts-online/refs/heads/main/userstatus.json"
 
 # Load the data
 try:
@@ -52,4 +52,5 @@ st.dataframe(df_latest[["user_display", "name", "date", "event", "time"]].rename
     "event": "Event",
     "time": "Time"
 }))
+
 
